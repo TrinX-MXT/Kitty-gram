8042c2085f2ff6d758cc81e4a212463043c3efbb
TrinXy <Maksonoid@gmail.com>
2026-04-26 17:54:50 +0300

    feat: Toast messages sys

--- Front/api_test.js
+++ Front/api_test.js
@@ -1,8 +1,20 @@
-fetch("http://localhost:5432/posts")
-.then(response => response.json())
-.then(json => console.log(json))
+const API_BASE_URL = 'http://localhost:8080';
 
-fetch("http://localhost:5432/users")
+fetch(`${API_BASE_URL}/posts`, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+    .then(response => response.json())
+    .then(json => console.log(json));
+
+
+fetch("http://localhost:8080/users")
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+fetch("http://localhost:8080/posts")
     .then(response => response.json())
     .then(json => console.log(json))
 
